d18b04dccbb3badc9a0fad133263d7ff073849dc
Sargunan <gunan24@gmail.com>
2022-01-09 20:24:44 +0530

    Renamed

--- Tutorial_Opencv_CMake/Hello_opencv.cpp
+++ Tutorial_Opencv_CMake/Hello_opencv.cpp
@@ -5,10 +5,12 @@ using namespace cv;
 int main(void) {
 	
 	// Read image in GrayScale mode
-	Mat image = imread("boat.jpg",0);
+	Mat image = imread("boat.jpg");
 
 	// Save grayscale image
 	imshow("Image",image);
+
+	waitkey(0);
 
 	return 0;
 }
